eb5e8e08d145a9ba87dd8fd298d79c5edc46ee1e
Marc R. <mriverolopez@gmail.com>
2026-07-03 06:09:22 +0200

    Table-drive content type detection

--- markdown_ingress/core/metadata_basic.py
+++ markdown_ingress/core/metadata_basic.py
@@ -4,6 +4,27 @@ from __future__ import annotations
 
 from selectolax.parser import HTMLParser
 
+_CONTENT_TYPE_SELECTORS = (
+    ("article", ("article", 'meta[property="og:type"][content="article"]', ".post", ".article")),
+    (
+        "documentation",
+        (".documentation", "#docs", ".docs", ".api-doc", ".api-docs", ".api-documentation"),
+    ),
+    (
+        "forum",
+        (
+            ".forum",
+            ".discussion",
+            ".thread",
+            ".comment",
+            ".comments",
+            ".comment-list",
+            ".comment-thread",
+        ),
+    ),
+    ("ecommerce", (".product", '[itemtype*="Product"]', ".price", ".add-to-cart")),
+)
+
 
 def first_meta_content(parser: HTMLParser, selectors: tuple[str, ...]) -> str | None:
     for selector in selectors:
@@ -29,41 +50,7 @@ def extract_keywords(parser: HTMLParser) -> str | None:
 
 
 def detect_content_type(parser: HTMLParser) -> str:
-    if (
-        parser.css_first("article")
-        or parser.css_first('meta[property="og:type"][content="article"]')
-        or parser.css_first(".post")
-        or parser.css_first(".article")
-    ):
-        return "article"
-
-    if (
-        parser.css_first(".documentation")
-        or parser.css_first("#docs")
-        or parser.css_first(".docs")
-        or parser.css_first(".api-doc")
-        or parser.css_first(".api-docs")
-        or parser.css_first(".api-documentation")
-    ):
-        return "documentation"
-
-    if (
-        parser.css_first(".forum")
-        or parser.css_first(".discussion")
-        or parser.css_first(".thread")
-        or parser.css_first(".comment")
-        or parser.css_first(".comments")
-        or parser.css_first(".comment-list")
-        or parser.css_first(".comment-thread")
-    ):
-        return "forum"
-
-    if (
-        parser.css_first(".product")
-        or parser.css_first('[itemtype*="Product"]')
-        or parser.css_first(".price")
-        or parser.css_first(".add-to-cart")
-    ):
-        return "ecommerce"
-
+    for content_type, selectors in _CONTENT_TYPE_SELECTORS:
+        if any(parser.css_first(selector) for selector in selectors):
+            return content_type
     return "webpage"
